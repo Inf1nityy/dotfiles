faf6c40969bd791f54aec19335738e0913a39ded
Inf1nityy <br0wn_rabbit@protonmail.com>
2025-01-15 23:35:17 +0100

    setup org roam and other things

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-spacegrey)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -41,6 +41,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/documents/org/")
+(setq org-roam-directory "~/documents/org")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -75,6 +76,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+;; dashboard dependencies
 (use-package! page-break-lines
   :ensure t
   :config
@@ -85,6 +87,7 @@
 (use-package! all-the-icons
   :if (display-graphic-p))
 
+;; dashboard setup
 (use-package! dashboard
   :ensure t
   :init
@@ -112,4 +115,22 @@
   :config
   (dashboard-setup-startup-hook))
 
+;; make dashboard show up for emacsclient windows
 (setq initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name)))
+
+;; make org roam not have a list of numbers at the start of a file name
+(setq org-roam-capture-templates
+      '(("d" "default" plain "%?"
+         :target (file+head "${slug}.org"
+                            "#+title: ${title}\n")
+         :unnarrowed t)))
+
+;; setup org roam ui
+(use-package! org-roam-ui
+  :after org-roam
+  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
+ '(org-agenda-files
+   '("~/documents/school/2024-2025/school.org" "/home/apricate/documents/org/nexia.org" "/home/apricate/documents/org/projects.org"))
  '(package-selected-packages '(nerd-icons dashboard page-break-lines all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
