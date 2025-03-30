439521a3872987bdebb2a31e0898ed7452aca684
Inf1nityy <br0wn_rabbit@protonmail.com>
2025-03-30 15:11:16 +0200

    change colors and font

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,20 @@
 function ColorMyPencils(color)
-	color = color or "nord"
+	color = color
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 end
 
 return {
-    'shaunsingh/nord.nvim',
+    'RRethy/base16-nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
-        transparency = true
+        telescope = true
     },
     config = function()
-        ColorMyPencils()
+        ColorMyPencils("base16-black-metal-gorgoroth")
     end,
 }
