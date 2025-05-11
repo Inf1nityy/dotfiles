cf275da7614a54670ae8cb466679a8f1081c00e3
Inf1nityy <br0wn_rabbit@protonmail.com>
2025-05-11 22:54:34 +0200

    change colorscheme and tmux leader key

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    'nyoom-engineering/oxocarbon.nvim',
+    'projekt0n/github-nvim-theme',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function ()
-        vim.cmd([[ colorscheme oxocarbon ]])
+        vim.cmd([[ colorscheme github_dark_default ]])
     end
 }
